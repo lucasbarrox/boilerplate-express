168ab024fcf64dc6a52b740e03c96d455de6c7ab
Lucas Oliveira <42456678+lucasbarrox@users.noreply.github.com>
2025-09-10 03:39:47 +0000

    Chain Middleware to Create a Time Server

--- myApp.js
+++ myApp.js
@@ -7,6 +7,13 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.json({ "time": req.time });
+});
+
 app.use("/public", express.static(__dirname + "/public"));
 
 
